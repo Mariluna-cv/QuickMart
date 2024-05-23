1a306b2a505958017bf8ba94fc88815af129e49e
Andruws <carlos.gonzalezcch@cun.edu.co>
2024-05-23 00:10:54 -0500

    up

--- pagina QuickMart/Conexion.php
+++ pagina QuickMart/Conexion.php
@@ -5,6 +5,14 @@ $user = "root";
 $pass = "";
 $DB = "quickmart";
 
+$Conexion = new mysqli($server, $user, $pass, $DB);
 
+if($Conexion->connect_errno){
+
+    die("conexion fallida" . $Conexion->connect_errno);
+} else {
+
+    echo"conectado";
+}
 
 ?>
